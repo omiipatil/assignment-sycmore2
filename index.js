02bb794d32e15f5d4909c4a09e2a54d4ff0b498f
omiipatil <92794706+omiipatil@users.noreply.github.com>
2022-06-08 18:29:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn=document.getElementById("btn");
+btn.onclick=function (){
+    
+        const element=document.getElementById("box")
+        const image=document.getElementById("img")
+        element.style.display = "none"
+        image.style.display = "block"
+}
+
